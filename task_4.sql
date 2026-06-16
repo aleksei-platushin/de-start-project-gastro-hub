d77f0850305c34e4963f69152eaa84b906b12965
Aleksei Platushin <zeldman.92@gmail.com>
2026-06-16 23:12:31 +1200

    Update task_4.sql
    Implement top 3 restaurants by pizza variety count.

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,18 @@
 /*добавьте сюда запрос для решения задания 4*/
+WITH all_kinds_of_pizzas AS (
+  SELECT 
+    r.restaurant_uuid,
+    r.name,
+    -- Извлекаем все ключи (названия пицц) из JSON-объекта 'Пицца' в виде отдельных строк
+    jsonb_object_keys(menu->'Пицца') AS pizza_type
+  FROM cafe.restaurants r
+  -- Фильтруем заведения, у которых есть категория 'Пицца' на верхнем уровне JSON
+  WHERE menu ? 'Пицца'
+)
+SELECT
+  name AS restaurant_name,
+  COUNT(pizza_type) AS pizza_type_count
+FROM all_kinds_of_pizzas
+GROUP BY restaurant_uuid, name
+ORDER BY pizza_type_count DESC
+LIMIT 3;
